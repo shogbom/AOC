0ecbbf185fda35bfcc32466978589589a4de6cd4
shogbom <samuel.aronson.hogbom@gmail.com>
2025-12-02 16:23:26 +0100

    d1 complete

---
+++ 25/d1/first.py
@@ -0,0 +1,21 @@
+#0  - 99
+count = 0
+curr = 50 # starting
+
+with open("list", "r") as f:
+    for line in f:
+        ist = line.strip()
+        ch = ist[0]
+        num = int(ist[1:])
+        
+        if ch == 'R':
+            curr += num
+        elif ch == 'L':
+            curr -= num
+        else: 
+            print("error")
+        
+        curr = curr % 100
+        if curr == 0:
+            count += 1
+    print(count)
