7657623d212b9f67801f707363b1cb65899a7483
shogbom <samuel.aronson.hogbom@gmail.com>
2025-12-05 17:19:04 +0100

    second d4 complete

---
+++ 25/d4/second.py
@@ -0,0 +1,40 @@
+removable = 0
+
+
+
+
+def isAcc(x0, y0, diagram): 
+    nbrAround = 0
+   
+    for dx in (-1, 0, 1):
+        for dy in (-1, 0, 1):
+            if dx == 0 and dy == 0: 
+              continue  # skip the center point
+
+            x = x0 + dx
+            y = y0 + dy
+            if (x >= 0 and x < len(diagram)) and (y >= 0 and y < len(diagram[x])):
+                if diagram[x][y] == '@':
+                    nbrAround += 1                                  
+    return nbrAround
+
+
+def nbrNegh(diagram):
+    count = 0
+    for x in range(len(diagram)):
+        for y in range(len(diagram[x])):
+            if diagram[x][y] == '@' :
+                if isAcc(x, y, diagram) < 4:
+                    count += 1
+                    diagram[x][y] = 'x'
+    return count
+
+with open("list", "r") as f:
+    diagram = [list(line.strip()) for line in f.readlines()]
+    stateChanged = True
+    while stateChanged:
+        removed = nbrNegh(diagram)
+        stateChanged = removed > 0
+        removable += removed
+
+print(removable)
